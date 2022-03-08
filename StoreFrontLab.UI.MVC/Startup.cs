d348e634c14e4e592dc59c0319e24974db99d7d6
Austin Wallace <auswallace@outlook.com>
2022-03-08 15:23:42 -0600

    Added security and user admin

--- StoreFrontLab.UI.MVC/Startup.cs
+++ StoreFrontLab.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(StoreFrontLab.UI.MVC.Startup))]
 namespace StoreFrontLab.UI.MVC
 {
     public partial class Startup
