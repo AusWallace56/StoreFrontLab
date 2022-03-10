dcc8095dd9699ac80ab1bb21718698b8ae1f6415
Austin Wallace <auswallace@outlook.com>
2022-03-09 21:26:24 -0600

    Added functioning contact form

--- StoreFrontLab.UI.MVC/Controllers/HomeController.cs
+++ StoreFrontLab.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using StoreFrontLab.UI.MVC.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
 
 namespace StoreFrontLab.UI.MVC.Controllers
 {
@@ -21,5 +25,53 @@ namespace StoreFrontLab.UI.MVC.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = $"{cvm.Name} has sent you the following message:<br />" +
+                    $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+
+                MailMessage mm = new MailMessage(
+                    
+                    ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                    ConfigurationManager.AppSettings["EmailTo"].ToString(),
+
+                    cvm.Subject,
+
+                    body
+
+                    );
+
+                mm.IsBodyHtml = true;
+
+                mm.Priority = MailPriority.High;
+
+                mm.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+                client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                    ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (System.Exception ex)
+                {
+                    ViewBag.CustomerMessage = $"We're sorry, but your request could not be completed at this time." +
+                        $"Please try again later. Error Message: <br /> {ex.StackTrace}";
+                    return View(cvm);
+                }
+                return View("EmailConfirmation", cvm);
+            }
+
+            return View();
+        }
+
     }
 }
